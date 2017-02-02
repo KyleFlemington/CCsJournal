7cf3e83f0da14abdded307ccf91789aa83a99f62
kyle <kyleflemington@gmail.com>
2017-02-02 12:18:50 -0800

    INIT: Express, Pug-template engine, ES5

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app 		= express();
 
+app.use('/static', express.static('public'));
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug');
+
+
 
 app.listen(4500, function(){
 	console.log('listening on port 4500')
@@ -8,6 +14,6 @@ app.listen(4500, function(){
 
 
 app.get('/', function(req, res){
-	res.send("hello redux and react")
+	res.render("index")
 });
 
